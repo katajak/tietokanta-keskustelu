b7d0668ca34badcc2108211383db2ab2e57d07b4
katajak <101673344+katajak@users.noreply.github.com>
2022-04-01 13:10:01 +0300

    Add new table 'areas'

--- schema.sql
+++ schema.sql
@@ -1,12 +1,19 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
-    password TEXT
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE areas (
+    id SERIAL PRIMARY KEY,
+    title TEXT
 );
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     user_id INTEGER REFERENCES users,
+    area_id INTEGER REFERENCES areas,
     sent_at TIMESTAMP
 );
