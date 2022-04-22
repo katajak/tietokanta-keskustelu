e8069c18f8f6240b620fb6a89d414f6ca04bc86e
katajak <101673344+katajak@users.noreply.github.com>
2022-04-22 15:08:27 +0300

    Update sql schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+SET TIMEZONE="Europe/Helsinki";
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS areas CASCADE;
+DROP TABLE IF EXISTS threads CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS likes CASCADE;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
@@ -11,11 +19,28 @@ CREATE TABLE areas (
     visible BOOLEAN
 );
 
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users,
+    area_id INTEGER REFERENCES areas,
+    sent_at TIMESTAMP,
+    visible BOOLEAN
+);
+
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     user_id INTEGER REFERENCES users,
     area_id INTEGER REFERENCES areas,
+    thread_id INTEGER REFERENCES threads,
     sent_at TIMESTAMP,
     visible BOOLEAN
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    message_id INTEGER REFERENCES messages,
+    liked BOOLEAN
 );
